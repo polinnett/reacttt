1314000a3920912f70711a8c41f46cb720c58b3e
polinnett <polina.hafizova04@mail.ru>
2024-05-15 20:29:18 +0300

    lab5 done

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      includeAssets: ["logo.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "react-blog",
+        short_name: "blog",
+        description: "blog desc",
+        theme_color: "#ffffff",
+        start_url: "/",
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+      },
+    }),
+  ],
+})
